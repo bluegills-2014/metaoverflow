502d32c05af74ae635b361226b8be92ec1944d24
Sean & willot <pair+nase00.willot@devbootcamp.com>
2015-01-14 14:13:02 -0600

    response model, migration

---
+++ db/migrate/20150114201048_create_responses.rb
@@ -0,0 +1,12 @@
+class CreateResponses < ActiveRecord::Migration
+  def change
+    create_table :responses do |t|
+      t.belongs_to :user
+      t.text :content
+      t.date :posted_at
+      t.references :respondable, polymorphic: true, index: true
+
+      t.timestamps
+    end
+  end
+end
