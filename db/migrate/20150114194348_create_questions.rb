16f4c1f48bd450620b1b0df3ca51e0fae3827551
Sean & willot <pair+nase00.willot@devbootcamp.com>
2015-01-14 13:47:24 -0600

    questions table done

--- db/migrate/20150114194348_create_questions.rb
+++ db/migrate/20150114194348_create_questions.rb
@@ -1,6 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.string :title
+      t.text :content
+      t.date :posted_at
+      t.belongs_to :user
 
       t.timestamps
     end
