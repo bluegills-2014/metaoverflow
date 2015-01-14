9949c0f8835212ee768e9097fa445ddac392d5de
Sean & willot <pair+nase00.willot@devbootcamp.com>
2015-01-14 14:21:48 -0600

    renamed question tags join table

---
+++ db/migrate/20150114201728_create_question_tags_join_table.rb
@@ -0,0 +1,8 @@
+class CreateQuestionTagsJoinTable < ActiveRecord::Migration
+  def change
+    create_table :question_tags do |t|
+      belongs_to :question
+      belongs_to :tag
+    end
+  end
+end
--- db/migrate/20150114201728_question_tags.rb
+++
@@ -1,4 +0,0 @@
-class QuestionTags < ActiveRecord::Migration
-  def change
-  end
-end
