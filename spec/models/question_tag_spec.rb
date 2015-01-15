5672fc0e2fe54ce3e0aff6fd8825f01ece3ec654
Nirag Amin & Jamie Pilgrim <pair+namin2.pilgrimmemoirs@devbootcamp.com>
2015-01-15 13:10:24 -0600

    Adding model tests

--- spec/models/question_tag_spec.rb
+++ spec/models/question_tag_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe QuestionTag, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:tag) }
+  it { should belong_to(:question) }
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:username) }
+  it { should have_many(:answers) }
+  it { should have_many(:questions) }
+  it { should have_many(:votes) }
+  it { should have_many(:responses) }
+  it { should have_many(:tags) }
+  it { should allow_value('text@email.com').for(:email) }
+  it { should_not allow_value('textemail.com').for(:email) }
+  it { should have_secure_password }
+
+
 end
