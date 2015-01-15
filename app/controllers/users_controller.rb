121dca4384af1f7073278710dcb344fc34e5fe4d
Nirag Amin & Jamie Pilgrim <pair+namin2.pilgrimmemoirs@devbootcamp.com>
2015-01-15 10:39:38 -0600

    Adding changes to user modle and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,19 @@ class UsersController < ApplicationController
   def show
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: 'User was successfully created.'
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_user
